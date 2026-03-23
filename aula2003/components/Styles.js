4e14f15c883017a928104e12920a41e7091d8022
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2026-03-23 11:58:26 -0400

    inicio da estilização

---
+++ aula2003/components/Styles.js
@@ -0,0 +1,29 @@
+const styles = StyleSheet.create({
+  /* tela: {
+    backgroundColor: "lightgray",
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "lightblue",
+    height: 50,
+  },
+  conteudo: {
+    flex: 1,
+  }, */
+  CardButtom: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  /* botao: {
+    position: "absolute",
+    bottom: 25,
+    right: 25,
+    backgroundColor: "red",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    elevation: 3,
+  }, */
+});
+
+export default styles;
