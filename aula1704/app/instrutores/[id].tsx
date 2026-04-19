da7c6386821ce224cf5be3ba95bc23a9b6302ca9
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2026-04-19 14:45:25 -0400

    finalizando a parte do instrutor

--- aula1704/app/instrutores/[id].tsx
+++ aula1704/app/instrutores/[id].tsx
@@ -1,3 +1,7 @@
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
+import { cursos, instrutores } from "../../data/dados.json";
+
 /* 
 Tela de Instrutor (/instrutores/[id])
 Requisitos:
@@ -9,3 +13,48 @@ Exibir:
 Nome do instrutor
 Cursos ministrados
 */
+
+export default function DetalheInstrutor() {
+  const router = useRouter();
+  const { id } = useLocalSearchParams();
+  const instrutor = instrutores.find((i) => i.id === id);
+  const cursosMinistrados = cursos.filter((c) => c.idInstrutor === id);
+
+  if (!instrutor || !cursosMinistrados) {
+    return (
+      <View style={{ padding: 20 }}>
+        <Text>Instrutor não encontrado</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View
+      style={{
+        gap: 10,
+        padding: 20,
+      }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        Nome do Instrutor: {instrutor.nome}
+      </Text>
+
+      <View
+        style={{
+          gap: 10,
+        }}
+      >
+        <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+          Cursos ministrados:
+        </Text>
+        {cursosMinistrados.map((curso) => (
+          <Button
+            key={curso.id}
+            title={curso.nome}
+            onPress={() => router.push(`/cursos/${curso.id}`)}
+          />
+        ))}
+      </View>
+    </View>
+  );
+}
